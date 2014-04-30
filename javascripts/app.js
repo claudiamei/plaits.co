b8e508c5f84d0183568b9687ae743661016a223d
Doug Kang <dougkang@gmail.com>
2014-04-29 18:48:35 -0700

    Adding hacky way of submitting google forms

--- javascripts/app.js
+++ javascripts/app.js
@@ -17,11 +17,29 @@ landingPageApp.config(['$routeProvider',
 }]);
  
  
-landingPageApp.controller('SignUpController', function($scope, $location) {
+landingPageApp.controller('SignUpController', [ '$scope', '$http', '$location', function($scope, $http, $location) {
   $scope.submit = function() {
-    // TODO submit form
-    $location.path('/thankyou')
+    var data = {
+      "entry.2004207529": $scope.firstName,
+      "entry.1724989784": $scope.lastName,
+      "entry.575036860": $scope.email,
+    }
+    $http({
+      method  : 'POST',
+      url     : 'https://docs.google.com/forms/d/1YIR735YkXc_Feq30uiD_sgr6ub50x2svatbA3k5ttpw/formResponse',
+      data    : $.param(data),
+      dataType: 'xml',
+      headers : { 
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+      .success(function(data) {
+        $location.path('/thankyou')
+      })
+      .error(function(data) {
+        $location.path('/thankyou')
+      });
   };
-});
+}]);
  
 landingPageApp.controller('ThankYouController');
