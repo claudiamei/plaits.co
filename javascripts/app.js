d3cfd522b1d3b19ac4bdbbf1d9849cb7d75451fb
Doug Kang <dougkang@gmail.com>
2014-04-30 19:27:30 -0700

    fixing issue where users were able to submit empty values through keyboard

--- javascripts/app.js
+++ javascripts/app.js
@@ -19,26 +19,24 @@ landingPageApp.config(['$routeProvider',
  
 landingPageApp.controller('SignUpController', [ '$scope', '$http', '$location', function($scope, $http, $location) {
   $scope.submit = function() {
-    var data = {
-      "entry.2004207529": $scope.firstName,
-      "entry.1724989784": $scope.lastName,
-      "entry.575036860": $scope.email,
-    }
-    $http({
-      method  : 'POST',
-      url     : 'https://docs.google.com/forms/d/1YIR735YkXc_Feq30uiD_sgr6ub50x2svatbA3k5ttpw/formResponse',
-      data    : $.param(data),
-      dataType: 'xml',
-      headers : { 
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    })
-      .success(function(data) {
-        $location.path('/thankyou')
+    if ($scope.submitForm.$valid) {
+      var data = {
+        "entry.2004207529": $scope.firstName,
+        "entry.1724989784": $scope.lastName,
+        "entry.575036860": $scope.email,
+      };
+      $http({
+        method  : 'POST',
+        url     : 'https://docs.google.com/forms/d/1YIR735YkXc_Feq30uiD_sgr6ub50x2svatbA3k5ttpw/formResponse',
+        data    : $.param(data),
+        dataType: 'xml',
+        headers : { 
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
       })
-      .error(function(data) {
-        $location.path('/thankyou')
-      });
+        .success(function(data) { $location.path('/thankyou') })
+        .error(function(data) { $location.path('/thankyou') });
+    }
   };
 }]);
  
